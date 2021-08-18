13d53be2db2549f5b0636b28b52a1c53ee417511
jose torres <jose.torres@nuvem.mx>
2021-08-18 17:55:10 -0500

    kluster feature | login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,10 +112,8 @@
                     </div>
                 </form>
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
+
+                    <a href="https://laracasts.com">Registrate</a>
                 </div>
             </div>
         </div>
